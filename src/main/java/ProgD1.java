cba8071522ec7a97eaff286482f9745d4456a0c0
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-07 16:53:48 +0900

    Update ProgD1.java

--- src/main/java/ProgD1.java
+++ src/main/java/ProgD1.java
@@ -1,9 +1,9 @@
 public class ProgD1 {
 
-	public static void main(String[] args) {
-		// ここにプログラムを追加
-		yamanote.printStations();
-	}
+    public static void main(String[] args) {
+        // ここにプログラムを追加
+        yamanote.printStations();
+    }
 
 }
 
